cc1697406b27bf75eaef79cb8db2f511a60a19e0
YGN <ygntechstartup@gmail.com>
2024-11-07 08:45:07 +0530

    changes done

--- target/debug/deps/os1.d
+++ target/debug/deps/os1.d
@@ -1,5 +1,5 @@
-C:\Users\gagan\Desktop\personal\Projects\rust_files\os1\target\debug\deps\os1.exe: src/main.rs
+C:\Users\gagan\Desktop\Personal\Projects\os1\target\debug\deps\os1.exe: src/main.rs
 
-C:\Users\gagan\Desktop\personal\Projects\rust_files\os1\target\debug\deps\os1.d: src/main.rs
+C:\Users\gagan\Desktop\Personal\Projects\os1\target\debug\deps\os1.d: src/main.rs
 
 src/main.rs:
--- target/thumbv7em-none-eabihf/debug/os1.d
+++ target/thumbv7em-none-eabihf/debug/os1.d
@@ -1,1 +1,1 @@
-C:\Users\gagan\Desktop\personal\Projects\rust_files\os1\target\thumbv7em-none-eabihf\debug\os1: C:\Users\gagan\Desktop\personal\Projects\rust_files\os1\src\main.rs
+C:\Users\gagan\Desktop\Personal\Projects\os1\target\thumbv7em-none-eabihf\debug\os1: C:\Users\gagan\Desktop\Personal\Projects\os1\src\main.rs
